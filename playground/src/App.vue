54a9f2d3816954f6f379943b30334bd7b89a95ed
mornir <jeromepott@hotmail.com>
2019-06-17 20:55:32 +0200

    Update HTML

--- playground/src/App.vue
+++ playground/src/App.vue
@@ -3,31 +3,33 @@
     <h1>Instagram Feed</h1>
 
     <vue-instagram :token="token"
-                   :count="9">
-      <template v-slot="{ posts, errorMsg, fetchMorePosts }">
+                   :count="count">
+      <template v-slot="{ posts, errorMsg, fetchMorePosts, areMorePages}">
 
         <section>
           <div id="instagram-grid">
-            <a v-for="post in posts"
-               :key="post.id"
-               :href="post.link"
-               target="_blank"
-               rel="noopener">
-              <article>
+
+            <article v-for="post in posts"
+                     :key="post.id">
+              <a :href="post.link"
+                 target="_blank"
+                 rel="noopener">
                 <img :src="post.images.low_resolution.url"
                      alt="instagram">
-                <h2 v-if="post.caption">{{ post.caption.text }}</h2>
-                <ul class="tags-list">
-                  <li v-for="tag in post.tags"
-                      :key="tag">#{{ tag }}</li>
-                </ul>
-              </article>
-            </a>
+              </a>
+              <h2 v-if="post.caption">{{ post.caption.text }}</h2>
+              <ul class="tags-list">
+                <li v-for="tag in post.tags"
+                    :key="tag">#{{ tag }}</li>
+              </ul>
+            </article>
+
             <pre v-if="errorMsg">{{ errorMsg }}</pre>
           </div>
           <div class="text-center">
             <button class="text-xl border-b-4 border-primary leading-none"
-                    @click="fetchMorePosts">More</button>
+                    @click="fetchMorePosts"
+                    v-if="areMorePages">More</button>
           </div>
         </section>
       </template>
@@ -44,6 +46,7 @@ export default {
   data() {
     return {
       token: '14685470797.1677ed0.86f3a5db081a4d24953f755ac97f30da',
+      count: 1,
     }
   },
   components: {
